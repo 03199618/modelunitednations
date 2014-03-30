91e19e462a0ab147f3395449fb2d0f0b6c8ca758
Jakob Zeitler <jakob.zeitler@broken-music.de>
2014-03-30 19:30:23 +0200

    Design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
+
+  def current_user
+    @current_user ||= session[:user_id] && User.find(session[:user_id])
+  end
 end
